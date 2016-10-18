5290cd7083408ce5a0e7080d68f6519e50f42e48
Lance Welsh <lpw@yahoo-inc.com>
2016-10-17 21:02:31 -0700

    command line parser

---
+++ command-line-parser.js
@@ -0,0 +1,50 @@
+// Converts an array of arguments into a key value object, regarind an arg starting with dash as a key.
+// Still presumes that arguments are separated from keys by a space, but that's not too bad anymore.
+// Presumes any argument starting with a dash is a key and can't be an value to previous key.
+function parseCommandLineArgs( args ) {
+	return args.reduce( ( argsObjInReduction, arg, index, args ) => {
+		const isArgKey = arg[0] === '-';
+		let isArgValue = false;
+
+		// replace any previously undefined value for arg keys with current arg,
+		// or true if they current arg also starts with a dash and is presumed to be another arg key
+		argsObjInReduction = Object.keys( argsObjInReduction ).reduce( ( argsObjInUndefinedReplacement, argKey ) => {
+			let argValue = argsObjInReduction[ argKey ];
+
+			if( argValue === undefined ) {
+				argValue = isArgKey ? true : arg;
+				isArgValue = !isArgKey;
+			}
+
+			return Object.assign( argsObjInUndefinedReplacement, { [ argKey ] : argValue } );
+		}, {} );
+
+		// if argument is not a key and not used as a value for previous key, collect it as an extra arg
+		if( !isArgKey && !isArgValue) {
+			argsObjInReduction = Object.assign( argsObjInReduction, {
+				_extraArgs : ( argsObjInReduction._extraArgs || [] ).concat( arg )
+			});
+		}
+
+		// if argument is a key, assign it a temporary value of undefined to be replaced later with next arg or true,
+		// or assign it to true if it's the last arg in list with no hope of being replaces later with true.
+		if( isArgKey ) {
+			argsObjInReduction = Object.assign( argsObjInReduction, {
+				[ arg ] : index === args.length - 1 ? true : undefined
+			});
+		}
+
+		return argsObjInReduction;
+	}, {} );
+}
+
+const args = [ '-a', 'b', '-c', '-dee', 'e', '-f' ];
+
+argsObj = parseCommandLineArgs( args );
+console.log('parseCommandLineArgs with', args, 'retured', argsObj );
+
+const assert = require('assert');
+assert( argsObj[ '-a' ] === 'b' );
+assert( argsObj[ '-c' ] === true );
+assert( argsObj[ '-dee' ] === 'e' );
+assert( argsObj[ '-f' ] === true );
